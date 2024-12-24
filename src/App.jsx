53ae7788eb5c244209991dcb26a899ba5c27f9bf
Daniel Urbina <edu211004@gmail.com>
2024-12-24 14:49:27 -0600

    render per category

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,53 @@
-import { BrowserRouter, Routes, Route } from 'react-router'
+import { BrowserRouter, Routes, Route, Link, useParams } from 'react-router'
+import { categories, recipes } from './mocks/recipes.json'
 
 function App() {
   return (
     <BrowserRouter>
+      <h1>Recipes App</h1>
       <Routes>
-        <Route path="/" element={<Recipes />} />
+        <Route path="/" element={<HomePage />} />
+        <Route path='/categories/:categoryName' element={<CategoryPage/>}/>
       </Routes>
     </BrowserRouter>
   )
 }
-const Recipes = () => {
-  return (
+
+const HomePage = () => {
+  return <div>
+    {categories.map(c=>(
+      <div key={c.id}>
+        <Link to={`/categories/${c.name}`}>{c.name}</Link>
+      </div>
+    ))}
+  </div>
+}
+
+const CategoryPage = ()=>{
+  const {categoryName} = useParams()
+
+  const categoryRecipes = recipes.filter(r=> r.category === categoryName)
+
+  return(
     <div>
-      <h2>Recipes App</h2>
+      <h2>{categoryName} Category</h2>
+      <ul>
+        {categoryRecipes.map(r=> (
+          <li key={r.id}>
+            <h3>Recipe: {r.title}</h3>
+            <b>{r.description}</b>
+            <div>
+            <p>Ingredients: </p>
+            <ul >
+              {r.ingredients.map(i=>(
+                <li key={i}>{i}</li>
+              ))}
+            </ul>
+            </div>
+          </li>
+        ))}
+      </ul>
     </div>
   )
 }
-
 export default App
